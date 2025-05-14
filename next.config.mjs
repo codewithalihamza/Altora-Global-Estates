1b8be3f00bde2de3f3653c6533f3b7b37c7ad547
codewithalihamza <512syedalihamza@gmail.com>
2025-05-14 23:32:53 +0500

    Add image remote patterns for Unsplash and configure ESLint to ignore during builds in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
